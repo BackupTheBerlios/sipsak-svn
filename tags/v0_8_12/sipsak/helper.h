6ca29853557d28e51a1fb228bcff8b5e0373d864
(no author) <(no author)@75b5f7c7-cfd4-0310-b54c-e118b2c5249a>
2005-04-24 02:44:14 +0000

    This commit was manufactured by cvs2svn to create tag 'v0_8_12'.

---
+++ tags/v0_8_12/sipsak/helper.h
@@ -0,0 +1,43 @@
+/*
+ * $Id: helper.h,v 1.6 2005/04/10 20:57:28 calrissian Exp $
+ *
+ * Copyright (C) 2002-2004 Fhg Fokus
+ * Copyright (C) 2004-2005 Nils Ohlmeier
+ *
+ * This file belongs to sipsak, a free sip testing tool.
+ *
+ * sipsak is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * sipsak is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef SIPSAK_HELPER_H
+#define SIPSAK_HELPER_H
+
+#ifdef HAVE_SYS_TIME_H
+# include <sys/time.h>
+#else
+# include <time.h>
+#endif
+
+long getaddress(char *host);
+
+void get_fqdn();
+
+void replace_string(char *mes, char *search, char *replacement);
+
+void insert_cr(char *mes);
+
+void swap_buffers(char *fst, char *snd);
+
+void trash_random(char *message);
+
+double deltaT(struct timeval *t1p, struct timeval *t2p);
+
+#endif
